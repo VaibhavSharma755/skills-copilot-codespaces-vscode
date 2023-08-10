aaee89f9478cb54594ec7401fed5f8570a692e82
Vaibhav Sharma <93032898+VaibhavSharma755@users.noreply.github.com>
2023-08-10 13:03:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberList = document.getElementById("memberList");
+  var skillsList = document.getElementById("skillsList");
+  member.style.display = "block";
+  skills.style.display = "none";
+  memberList.style.color = "#000";
+  skillsList.style.color = "#fff";
+}
